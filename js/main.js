8ce0faa18546952dea407b8d1b2427a6a0469b76
subin <sbyoon92@gmail.com>
2019-05-01 20:03:15 +0900

    190501_subin_commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){ 
+    
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: true,
+        slideshow:true,
+        //itemWidth: 210,
+        itemMargin: 0,
+        minItems: 1,
+        maxItems: 1,
+        animationDuration: 500,
+        move:1,
+        slideshowSpeed:1000,
+        controlNav:true,
+        directionNav:false,
+    });
+    
+}); //document ready END 
