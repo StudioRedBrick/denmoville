336e5d2fe16f7dd63b8561de6ba6995895d05eb0
subin <sbyoon92@gmail.com>
2019-05-20 16:56:09 +0900

    subin commit

--- js/product_detail.js
+++ js/product_detail.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
     modalHeight();
     openDetail();
     closeDetail();
-    setWidth();
 }); //document ready END 
 
 function modalHeight(){
-    
     if(window.matchMedia("(min-width: 416px) and (max-width: 768px)").matches){
         $(".detail_wrap .fix .inner_wrap .left").css({"height":"407px"}); 
 
@@ -21,9 +19,54 @@ function modalHeight(){
 }
 
 function openDetail(){
+    
+    var widthVal = 0;
+    var fileExt = {},
+        fileExt1=".png";
+    
     $(".wrap .product_wrap li").on('click',function(){
         $(".detail_wrap").fadeToggle(100);
         $(".wrap").css({"display":"none"});
+        
+        var folderName = $(this).text();
+        $(".detail_wrap .fix .inner_wrap .right h3").append(folderName);
+        
+        $.ajax({
+            //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+            url: '../img/product_detail/'+folderName+'/',
+            success: function (data) {
+               //List all png or jpg or gif file names in the page
+               $(data).find("a:contains(" + fileExt1 + ")").each(function () {
+                   var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+                   
+                   $(".detail_wrap .fix .inner_wrap .left").append( "<li></li>");
+               });
+                
+                var elLength = $(data).find("a:contains(" + fileExt1 + ")").length;
+
+                setWidth(elLength);
+            } 
+        });
+        
+        function setWidth(elLength){
+            for(var i=1; i<elLength+1; i++){
+                var $imgSrc = "./img/product_detail/"+folderName+"/"+i+".png";
+
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+") ").append('<img src="">');
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+") img").attr('src',$imgSrc);
+            }
+
+            var getLength = $(".detail_wrap .fix .inner_wrap .left li").find('img').length;
+                console.log("getLength"+getLength);
+
+            if(window.matchMedia("(min-width: 416px) and (max-width: 768px)").matches){
+                widthVal = getLength*562;
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }else if(window.matchMedia("(min-width: 300px) and (max-width: 415px)").matches){
+                widthVal = getLength*270; 
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }
+        }
     });
 }
 
@@ -31,35 +74,8 @@ function closeDetail(){
     $(".detail_wrap .fix .inner_wrap .close_btn").on('click',function(){
         $(".detail_wrap").fadeToggle(100);
         $(".wrap").css({"display":"block"});
+        
+        $(".detail_wrap .fix .inner_wrap .left li").remove();
+        $(".detail_wrap .fix .inner_wrap .right h3").empty();
     });
-}
-
-function setWidth(){
-    if(window.matchMedia("(min-width: 416px) and (max-width: 768px)").matches){
-        console.log('tablet');
-        var getLength = $(".detail_wrap .fix .inner_wrap .media_flow .left li").length;
-        var widthVal = getLength*562;
-
-        $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
-        
-        $(window).resize(function(){
-            var getLength = $(".detail_wrap .fix .inner_wrap .media_flow .left li").length;
-            var widthVal = getLength*562;
-
-            $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal}); 
-        });
-    }else if(window.matchMedia("(min-width: 300px) and (max-width: 415px)").matches){
-        console.log('mobile');
-        var getLength = $(".detail_wrap .fix .inner_wrap .media_flow .left li").length;
-        var widthVal = getLength*270;
-
-        $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
-        
-        $(window).resize(function(){
-            var getLength = $(".detail_wrap .fix .inner_wrap .media_flow .left li").length;
-            var widthVal = getLength*270;
-
-            $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal}); 
-        });         
-    }
 }
