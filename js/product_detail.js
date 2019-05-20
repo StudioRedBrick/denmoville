cf19ed38de2dbf3b62a67236f7074fe105cfc4e3
subin <sbyoon92@gmail.com>
2019-05-20 20:34:51 +0900

    subin commit

--- js/product_detail.js
+++ js/product_detail.js
@@ -34,16 +34,68 @@ function openDetail(){
         $(".detail_wrap .fix .inner_wrap .right h3").append(folderName);
         
         console.log(finfolderName);
+        if(finfolderName=='ENOVA_AQUACLEAN'){
+            //for product with 6 images 
+            for(var i=1; i<=6; i++){
+                var $imgSrc = "./img/product_detail/"+finfolderName+"/"+i+".png";
 
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+")").append('<img src="">');
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+") img").attr('src',$imgSrc);
+            }
+            
+            var getLength = $(".detail_wrap .fix .inner_wrap .left li").find('img').length;
+            
+            if(window.matchMedia("(min-width: 416px) and (max-width: 768px)").matches){
+                widthVal = getLength*562;
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }else if(window.matchMedia("(min-width: 300px) and (max-width: 415px)").matches){
+                widthVal = getLength*270; 
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }
+        }else if(finfolderName=='PANDORA_AQUACLEAN'){
+            //for product with 7 images 
+            for(var i=1; i<=7; i++){
+                var $imgSrc = "./img/product_detail/"+finfolderName+"/"+i+".png";
+
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+")").append('<img src="">');
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+") img").attr('src',$imgSrc);
+            } 
+            
+            var getLength = $(".detail_wrap .fix .inner_wrap .left li").find('img').length;
+            
+            if(window.matchMedia("(min-width: 416px) and (max-width: 768px)").matches){
+                widthVal = getLength*562;
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }else if(window.matchMedia("(min-width: 300px) and (max-width: 415px)").matches){
+                widthVal = getLength*270; 
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }
+        }else{
+            //for product with 5 images
+            for(var i=1; i<=5; i++){
+                var $imgSrc = "./img/product_detail/"+finfolderName+"/"+i+".png";
+
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+")").append('<img src="">');
+                $(".detail_wrap .fix .inner_wrap .left li:nth-child("+i+") img").attr('src',$imgSrc);
+            } 
+            
+            var getLength = $(".detail_wrap .fix .inner_wrap .left li").find('img').length;
+            
+            if(window.matchMedia("(min-width: 416px) and (max-width: 768px)").matches){
+                widthVal = getLength*562;
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }else if(window.matchMedia("(min-width: 300px) and (max-width: 415px)").matches){
+                widthVal = getLength*270; 
+                $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
+            }
+        }
+
+/*
         $.ajax({
-            //This will retrieve the contents of the folder if the folder is configured as 'browsable'
             url: '/img/product_detail/'+finfolderName+'/',
             success: function (data) {
                //List all png or jpg or gif file names in the page
                $(data).find("a:contains(" + fileExt1 + ")").each(function () {
-                   /*var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-                   console.log(filename);*/
-                   
                    $(".detail_wrap .fix .inner_wrap .left").append("<li></li>");
                });
                 
@@ -53,7 +105,7 @@ function openDetail(){
                 setWidth(elLength);
             } 
         });
-        
+
         function setWidth(elLength){
             for(var i=1; i<elLength+1; i++){
                 var $imgSrc = "./img/product_detail/"+finfolderName+"/"+i+".png";
@@ -72,6 +124,7 @@ function openDetail(){
                 $(".detail_wrap .fix .inner_wrap .left").css({"width":widthVal});
             }
         }
+*/
     });
 }
 
@@ -80,7 +133,7 @@ function closeDetail(){
         $(".detail_wrap").fadeToggle(100);
         $(".wrap").css({"display":"block"});
         
-        $(".detail_wrap .fix .inner_wrap .left li").remove();
+        $(".detail_wrap .fix .inner_wrap .left li img").remove();
         $(".detail_wrap .fix .inner_wrap .right h3").empty();
     });
 }
