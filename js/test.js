3d23a3e7d202cc439af64cbea2d277e5a2eb4ff6
subin <sbyoon92@gmail.com>
2019-07-01 19:03:06 +0900

    jjjjj

--- js/test.js
+++ js/test.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         PageHeight: 600,
         CoverWidth: 420,
         CoverHeight: 640,
+        VerticalMode:true,
         //PageDataFile: "demobook.html",
         Copyright: Key.Copyright,
         Key: Key.Key
